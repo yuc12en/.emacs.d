c68b47a2845d8ac06975ce62d804b9a5cc6b7420
yuc12en <yuchenxiao2002@outlook.com>
2022-10-10 12:13:25 +0800

    only Agenda lisp snippets init.el tracked

--- elpa/goto-line-preview-20220704.630/goto-line-preview-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from goto-line-preview.el  -*- no-byte-compile: t -*-
-(define-package "goto-line-preview" "20220704.630" "Preview line when executing `goto-line` command" '((emacs "25")) :commit "ee1cb651c021d1cab1a9f2577fa76d54da894037" :authors '(("Shen, Jen-Chieh" . "jcs090218@gmail.com")) :maintainer '("Shen, Jen-Chieh" . "jcs090218@gmail.com") :keywords '("convenience" "line" "navigation") :url "https://github.com/emacs-vs/goto-line-preview")
