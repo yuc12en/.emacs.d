2a29383579fda55eef7e22acba000d543193a386
yuc12en <yuchenxiao2002@outlook.com>
2022-10-15 07:43:58 +0800

    adding configs

--- init.el
+++ init.el
@@ -4,17 +4,22 @@
 
 ;; Interface and basic settings
 (setq inhibit-startup-message t)
+; line numbers and column numbers, ignore them when in org, term and eshell
 (column-number-mode 1)
 (global-display-line-numbers-mode 1)
-(scroll-bar-mode -1)
+(dolist (mode '(term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(Scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (tooltip-mode -1)
 (visual-line-mode 1)
 (setq visible-bell t)
-(load-theme 'wombat)
+(load-theme 'doom-dracula t)
 
-; Make ESC quit prompts
+;; key bindings 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; package initializing
@@ -62,7 +67,6 @@
 ;; Agenda
 (setq org-agenda-files '("~/.emacs.d/Agenda"))
 (global-set-key (kbd "C-c a") 'org-agenda)
-;;; 
 
 
 ;; yasnippet
@@ -73,20 +77,47 @@
   :config
   (yas-global-mode 1))
 
+;; WichKey
+(use-package which-key
+  :diminish
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 0.3))
 ;; amx
 (use-package amx
   :ensure t
   :init (amx-mode))
 
+;; helpful
+(use-package helpful
+  :custom
+  (ounsel-decribe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-fubction] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;;doom-themes
+(use-package doom-themes)
+
+;;; all-the-icons
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p)
+  :config
+  (add-to-list 'load-path "~/.emacs.d/icon"))
 ;; rainbow-delimiters
 (use-package rainbow-delimiters
-  :ensure t
   :hook (prog-mode . rainbow-delimiters-mode))
-
+;;ivyRich
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
 
 ;; highlight symbol
 (use-package highlight-symbol
-  :ensure t
   :init (highlight-symbol-mode)
   :bind ("C-c h" . highlight-symbol))
 
@@ -115,10 +146,20 @@
   :ensure t
   :bind
 (("C-x o" . 'ace-window)))
+;;Helpful
+(use-package )
 
-;;; ivy
+;; counsel M-o to some defined function
 (use-package counsel
-  :diminish)
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil))
+
+;ivy
 (use-package ivy
   :diminish
   :init
@@ -136,7 +177,7 @@
    ("C-x C-@" . 'counsel-mark-ring)
    ("C-x C-SPC" . 'counsel-mark-ring)
    :map minibuffer-local-map
-   ("C-r" . counsel-minibuffer-history))
+   ("C-r". counsel-minibuffer-history))
   :config
    (ivy-mode 1))
 
@@ -185,12 +226,6 @@
   :hook
   (prog-mode . flycheck-mode))
 
-;;; all-the-icons
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p)
-  :config
-  (add-to-list 'load-path "~/.emacs.d/icon"))
 ;(use-package org-roam
 ;  :ensure t
 ;  :custom
@@ -221,13 +256,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c4cecd97a6b30d129971302fd8298c2ff56189db0a94570e7238bc95f9389cfb" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "d8cec8251169ccfe192aa87d69b9378bc81599330f31498f85deaef633721302" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
+   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "fc2fa0ef550cbbf5d4d0a0e8b208d0dc75110918c9ad00823439b86748669736" "c4cecd97a6b30d129971302fd8298c2ff56189db0a94570e7238bc95f9389cfb" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "d8cec8251169ccfe192aa87d69b9378bc81599330f31498f85deaef633721302" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(org-agenda-files
-   '("d:/Emacs/.emacs.d/Agenda/dayview.org" "d:/Emacs/.emacs.d/Agenda/inbox.org" "d:/Emacs/.emacs.d/Agenda/workout.org") t)
+   '("d:/Emacs/.emacs.d/Agenda/dayview.org" "d:/Emacs/.emacs.d/Agenda/inbox.org" "d:/Emacs/.emacs.d/Agenda/workout.org"))
  '(package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(use-package snippet quote
-      (smart-mode-line-powerline-theme rainbow-delimiters highlight-symbol dashboard good-scroll smart-mode-line undo-tree mwim ace-window amx counsel monokai-theme yasnippet window-numbering use-package org-roam neotree monokai-pro-theme ivy goto-line-preview flycheck evil dracula-theme beacon atom-one-dark-theme all-the-icons))))
+   '(general helpful ivy-rich doom-themes which-key doom-modeline use-package snippet quote
+	     (smart-mode-line-powerline-theme rainbow-delimiters highlight-symbol dashboard good-scroll smart-mode-line undo-tree mwim ace-window amx counsel monokai-theme yasnippet window-numbering use-package org-roam neotree monokai-pro-theme ivy goto-line-preview flycheck evil dracula-theme beacon atom-one-dark-theme all-the-icons))))
